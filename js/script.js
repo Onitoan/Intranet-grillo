82bde995b2668760fac9e685819f348e913ca112
Antonio Inácio <84487731+Onitoan@users.noreply.github.com>
2022-11-24 11:27:10 -0300

    Add files via upload
    add functions to JS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-const beginning = document.querySelector('.Beginning');
-const btnAcess = document.querySelector('.bt-acess')
-
+const beginning = document.querySelector(".Beginning");
+const btnAcess = document.querySelector(".bt-acess");
+const help = document.querySelector(".help");
+const logout = document.querySelector(".logout");
+const subMenu = document.querySelector(".sub-menu");
+const notice = document.querySelector(".notice");
 
 const acess = () => {
-    beginning.classList.remove('open')
-}
+  beginning.classList.remove("open");
+  help.classList.add("open");
+  logout.classList.add("open");
+  subMenu.classList.add("open");
+  notice.classList.add("open");
+};
 
-btnAcess.addEventListener('click', acess)
+const exit = () => {
+  beginning.classList.add("open");
+  help.classList.remove("open");
+  logout.classList.remove("open");
+  subMenu.classList.remove("open");
+  notice.classList.remove("open");
+};
+
+btnAcess.addEventListener("click", acess);
+logout.addEventListener('click', exit);
